109b35cf3236a4e0a80c975118820be856176ff5
Myxazaebyxa <97283442+Myxazaebyxa@users.noreply.github.com>
2026-06-18 15:18:32 +0300

    Add files via upload

---
+++ CalculiDuty.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class CalculiDuty {
+    public static String result;
+    public static void Scanner() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Здравствуйте");
+        System.out.print("Введите вес (в кг.)");
+        int weight = scanner.nextInt();
+        System.out.print("Введите сумму(в руб.");
+        int price = scanner.nextInt();
+        System.out.print("Сумма пошлины:");
+        Duty(weight, price);
+    }
+
+    public static int Duty(int weight, int price) {
+        int result = weight * 100 + price / 100;
+        return result;
+    }
+    public static void main(String[] args) {
+        Scanner();
+        System.out.println(result);
+    }
+}
